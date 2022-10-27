732d57dd6f53308d4f1a427ceae9d57ab4bcb607
2pro4younoob <80782960+2pro4younoob@users.noreply.github.com>
2022-10-27 13:01:42 +0530

    The issue was on line 7 and it't now solved.

--- Instagram Followers.py
+++ Instagram Followers.py
@@ -4,7 +4,7 @@ def followers(username):
     r=requests.get(url).text
     start='"edge_followed_by":{"count":'
     end='},"followed_by_viewer"'
-    followers=str(r[r.find(0)+len(start):r.rfind(end)])
+    followers=str(r[r.find(start)+len(start):r.rfind(end)])
     result=f"{username} currently have {int(followers):,}followers."
     return result
 
